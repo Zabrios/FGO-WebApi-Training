177ca73f4a38dfddb21bc9f64a1a3f9beab251cd
Zabrios <dinocren@gmail.com>
2019-12-23 10:11:15 +0100

    [FGO WebApi Training] First commit, non-functional

---
+++ FGO.WebApi.Domain.Entities/Models/ServantBaseModel.cs
@@ -0,0 +1,22 @@
+﻿namespace FGO.WebApi.Domain.Entities
+{
+    public class ServantBaseModel
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string[] Alias { get; set; }
+        public int Cost { get; set; }
+        public string ServantClass { get; set; }
+        public int MaxLvl { get; set; }
+        public int RarityNum { get; set; }
+        public string RarityName { get; set; }
+        public int AtkLv1 { get; set; }
+        public int AtkMaxLv { get; set; }
+        public int AtkLv100 { get; set; }
+        public int HpLv1 { get; set; }
+        public int HpMaxLv { get; set; }
+        public int HpLv100 { get; set; }
+
+    }
+}
---
+++ FGO.WebApi.Persistence/Context/FGOContext.cs
@@ -0,0 +1,32 @@
+﻿using FGO.WebApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FGO.WebApi.Persistence.Context
+{
+    public class FGOContext : DbContext
+    {
+
+        public DbContext Instance => this;
+
+        public virtual DbSet<ServantBaseModel> Servants { get; set; }
+
+        protected FGOContext() { }
+        public FGOContext(DbContextOptions<FGOContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                throw new Exception("Database not properly configured");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FGOContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
